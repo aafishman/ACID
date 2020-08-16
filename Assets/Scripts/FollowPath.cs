fab950bfd36bdddf796952130c4c343dd146013b
aafishman <59353926+aafishman@users.noreply.github.com>
2020-08-16 01:53:58 -0500

    Make new sprite that moves along path nodes by hitting space bar. Change from diagonal pathfinding to 4 dir moving. Notes

--- Assets/Scripts/FollowPath.cs
+++ Assets/Scripts/FollowPath.cs
@@ -1,18 +1,98 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.Tilemaps;
+using Toolbox;
 public class FollowPath : MonoBehaviour
 {
     // Start is called before the first frame update
+    List<Vector3> path;
+    LevelBuilder levelBuilder;
+
+    public Vector3 cntLoc;
     void Start()
     {
-        
+        levelBuilder = GetComponent<LevelBuilder>();
+        StartCoroutine (Wander());
     }
+    Vector3 crntPathStart;
+    Vector3 crntPathEnd;
+    public bool pathReset = false;
+    public int crntPathIndex = 0;
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown("space"))
+        {
+            advance = true;
+                        print("space key was pressed");
+
+        }
+        else advance = false;
+
+        //move these to fixed or on coroutine\
+        //names are confusing, should all just be path
+        //some o these can be eliminated likely
+
+        //crntPathStart = levelBuilder.startCircle.position;
+
+        //crntPathStart = levelBuilder.endCircle.position;
+        //path
+
+        if (levelBuilder.crntPath != null && path != levelBuilder.crntPath) //if path has changed
+        {
+            crntPathStart = levelBuilder.startCircle.position;
+            crntPathStart = levelBuilder.endCircle.position;
+            path = levelBuilder.crntPath; //set to new path (this is inefficient listener whatever fix 4 real guy)
+            pathReset = true;  //prolly won't need, index thing below is better...
+            crntPathIndex = 0;
+
+        }
+
+        //     if (levelBuilder.crntPath != path)
+        // {
+
+        // }            path = levelBuilder.crntPath;
+        // }
+        // }
     }
+    //need a reset for path stuff like when the path actually changes...
+    //just also set if crnt path changes, doing it dumb anyawys right now...
+    //just have this calculated per movement anyways...
+    //should still use the linepath renderer stuff too tho
+    public bool advance = true;
+    IEnumerator Wander()
+    {
+        while (true)
+        {
+            if (advance == true)
+            // while (advance == true)
+            {
+                //walk along path
+                //advance location along path positions
+                if (crntPathIndex + 1 < path.Count)
+                {
+                    //advance damn you!
+                    transform.position = path[crntPathIndex];
+                }
+                //crntPathIndex += 1;
+                yield return new WaitForEndOfFrame();
+            }
+        }
+        //yield return new WaitForSeconds()
+    }
+
+    // //public void Wander()
+    // {
+    //     // foreach 
+    //     // transform.Translate(path([i]);\
+    //     //start at the beginning of the path
+    //     //start walking towards the end point, along the path
+    //     //stop at the end of the path or whatever
+    //     //decide how to do the tick movement - if it is everyone on screen moves per tick and moves a distance
+    //         //or is it people don't all move on the same tick structure
+    //         //the first idea is the only one that will work if people don't all move at the same time
+    //         //yeah going with this for now
+    // }
 }
--- Assets/Scripts/LevelBuilder.cs
+++ Assets/Scripts/LevelBuilder.cs
@@ -23,6 +23,10 @@ public class LevelBuilder : MonoBehaviour
     List<Vector3Int> wallTiles;
     List<Vector3Int> groundTiles;
 
+    public bool waitForWanderer = true;
+    //feature flag above to switch it to not regenerating the map unless the wandere has made it to the end
+    //will split off into its own thing for a person moving testing etc.
+
     void Start()
     {
         tilemap = GetComponent<Tilemap>();
@@ -51,10 +55,105 @@ public class LevelBuilder : MonoBehaviour
         }
 
         StartCoroutine(NextPath());
+        //StartCoroutine(TickMove());
+    }
+
+
+//function to go through a path and insert nodes at all grid intersection points
+//or in the center of each grid tile that it goes through
+
+public void InsertPathInters(Vector3 path)
+{
+//takes a path and returns a path with all the interesections spliced in to use to travel and stuff so its not just the nodes
+//so what, draw lines, check for tiles in the way... how would I do that... is that the best way to do it?
+//could just move something real fast along the path and try to see what tiles on the grid are picked up?
+//or do it so that the person moves towards the next way point, but get stopped at each center of new grid they get to?
+//that should be better? Otherwise storing huge paths for people all the time...could be huge differences/distances, so many?
+//can't think of way to just generate without tracking soemthing moving on it anyawys? So this way at least just one thing moving?
+//hrm so...what it checks at each spot if it should stop or not based on its movement speed? Since tick based?
+
+//ok so some movement questions
+//does diagonal cost same as one more or is it two?
+//its not along a path right? it should just be each actual grid?
+//can do both in actuality...
+
+//can advance them a long the path an amount relative to their speed OR
+//advance them only in factors of grid, 0,1,2,3 and do something like use the remainder to decide if they get to advance in addition to the integer
+//.8 movement speed, initial tick no move, next tick (.8 + .8 =1.6) is one square since over 1, next tick is (.6 +.8) = (1.4) so one more, then .4 +.8 = 1.2, then .2 + .8 = 1, then 0 = .8 = ZERO MOVE THIS TIME NICE
+//1.4 movement speed, initial tick 1 move, next tick .4 + 1.4 = (1.8), one move, next tick is .8 + 1.4 = 2.2 so TWO MOVES (or spaces or grids or whatever), next one is .2 + 1.4 = 1.6 so one move ETC!!!
+//yeah i kind of like this, it lets you try to time things to lol i like it yeah lets do that...
+
+
+}
+
+
+    public bool triggerNew = false;
+
+    public GameObject wanderer; //the object that wanders.... or should this just be the damn sprite lol....
+    //yeah lets just make it the sprite
+    //should justh ave had that the whole time....
+
+    //lol k
+    //just add a 'move the sprite along the path thing...'
+    //smae problem - is it continues or ticks per space or something
+    //yeah still do the space thing?
+public int crntIndex;
+    // IEnumerator TickMove()
+    // {
+    //     while (true)
+    //     {
+    //         //get the crnt index we on
+    //         //lol now i want it back on its own script ugh.
+    //         wanderer.transform.position = crntPath[crntIndex];
+    //         // print("wanderer.transform.position is: " + wanderer.transform.position);
+    //         // print(crntPath.Count);
+
+    //         if (crntIndex + 1 < crntPath.Count)
+    //         {
+    //             crntIndex += 1;
+    //             // if (triggerNew == true)
+    //             // {
+    //             //     crntIndex = 0;
+    //             //     triggerNew = false;
+    //             // }
+    //         }
+    //         yield return new WaitForSeconds(.05f);
+    //     }
+    // }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            wanderer.transform.position = crntPath[crntIndex];
+            // print("wanderer.transform.position is: " + wanderer.transform.position);
+            // print(crntPath.Count);
+
+            if (crntIndex + 1 < crntPath.Count)
+            {
+                crntIndex += 1;
+                // if (triggerNew == true)
+                // {
+                //     crntIndex = 0;
+                //     triggerNew = false;
+                // }
+            }
+        }
+    }
+    
+    public void WandererInit()
+    {
+      //  triggerNew = true;
+        crntIndex = 0;
+        wanderer.transform.position = crntPath[crntIndex];
+
+
     }
 
     IEnumerator NextPath()
     {
+        while (true)
+        {
         Vector3 start;
         Vector3 end;
         List<Vector3> path;
@@ -66,7 +165,7 @@ public class LevelBuilder : MonoBehaviour
             List<Vector3Int> list = findPathOpenCell ? groundTiles : wallTiles;
             end = tilemap.GetCellCenterWorld(RandomElement(list));
 
-            path = AStar.FindPathClosest(tilemap, start, end);
+           path = AStar.FindPathClosest(tilemap, start, end); //but how could I get the path of all nodes, like more ineffient in the code this comes from?
         } while (start == end || path == null || path.Count < minPathLength);
         path.Add(end);
 
@@ -76,9 +175,18 @@ public class LevelBuilder : MonoBehaviour
         startCircle.position = start;
         endCircle.position = end;
 
+        //triggerNew = true;
+        WandererInit();
         yield return new WaitForSeconds(duration);
-
-        StartCoroutine(NextPath());
+      //  print ("crnt index is: " +crntIndex);
+        print ("path count is: " + crntPath.Count);
+        //if (waitForWanderer == true && crntIndex == (crntPath.Count -1))//waits until the person gets to the end to restart - could add another sleep timer here if always want it to wait or something...
+        
+        //if (crntIndex == crntPath.Count)
+        //StartCoroutine(NextPath()); //doesn't this actually restart it? Should be a while loop instead... This makes a NEW one dummies! Why people still do this? I'm not wrong... am i? amiii?
+        
+        //just make it a while loop when we actulaly do sopmethign with it
+    }
     }
         public List<Vector3> crntPath;
 

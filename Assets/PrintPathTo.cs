81d1e63530fa2a94af95a7e5a6a0477522a81e6c
aafishman <59353926+aafishman@users.noreply.github.com>
2020-08-15 22:55:00 -0500

    first commit

---
+++ Assets/PrintPathTo.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+using Toolbox;
+
+public class PrintPathTo : MonoBehaviour
+{
+    //public void ListPath
+    public Tilemap tilemap;
+    public Vector3 startPos;
+    public Vector3 endPos;
+    // Start is called before the first frame update
+
+    
+    void Start()
+    {
+    startPos = gameObject.transform.position;    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //List<Vector3> path = AStar.FindPath(tilemap, startPos, endPos);
+        //print ("path is: " + path[0]);
+        print (AStar.FindPath(tilemap, startPos, endPos));
+    }
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+# Pathfinding
+Find paths in Unity Tilemaps with A* Search based off Amit Patel's implementation.
+
+Download and Import the Pathfinding [package](https://github.com/antonpantev/pathfinding/raw/master/Pathfinding.unitypackage) then use `AStar.FindPath()` like so:
+
+```c#
+List<Vector3> path = AStar.FindPath(tilemap, startPos, endPos);
+```
+
+<img src="https://raw.githubusercontent.com/antonpantev/pathfinding/master/PreviewImages/ScreenShot.png">
+TestCommit! WHY ARE THERE NO CHANGES TO COMMIT? THIS IS A CHANGE>ll.;[sdkaklsdjql;asec]

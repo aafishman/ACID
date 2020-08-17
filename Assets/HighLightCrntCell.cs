50c700bcad5bae6e3e424ecdc7e7b6b7a429cee9
aafishman <59353926+aafishman@users.noreply.github.com>
2020-08-16 20:55:44 -0500

    various items and starting scripts

---
+++ Assets/HighLightCrntCell.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.Tilemaps;
+ 
+public class HighLightCrntCell : MonoBehaviour
+{
+    public Tile highlightTile;
+    public Tilemap highlightMap;
+ 
+    private Vector3Int previous;
+ 
+    // do late so that the player has a chance to move in update if necessary
+    private void LateUpdate()
+    {
+        // get current grid location
+        Vector3Int currentCell = highlightMap.WorldToCell(transform.position);
+        // add one in a direction (you'll have to change this to match your directional control)
+        //currentCell.x += 1;
+        //lol this was just the direction, just get rid of to do under the char...
+        //could use simple thing like this to start for picking where you are going to move next..
+ 
+        // if the position has changed
+        if(currentCell != previous)
+        {
+            // set the new tile
+            highlightMap.SetTile(currentCell, highlightTile);
+ 
+            // erase previous
+            highlightMap.SetTile(previous, null);
+ 
+            // save the new position for next frame
+            previous = currentCell;
+        }
+    }
+}
---
+++ Assets/HighlighterMouseTile.cs
@@ -0,0 +1,55 @@
+﻿// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+// using UnityEngine.Tilemaps;
+// public class HighlighterMouseTile : MonoBehaviour
+// {
+//     public Tilemap highlightMap;
+//        // public    Tilemap tilemap;
+
+//     public TileBase highlightTile;
+
+//     void Start()
+//     {
+//         Vector3Int previousTileCoordinate = Vector3Int.zero;
+
+//     }
+//     void Update()
+//     {
+//          //set the previously highlighted tile back to null
+//          if(previousTileCoordinate != null)
+//          {
+//              highlightMap.SetTile(previousTileCoordinate, null);
+//          }
+ 
+ 
+//          //We need to highlight the tile on mousover. First get the curent position
+//          Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+//          Vector3Int tileCoordinate = highlightMap.WorldToCell(mouseWorldPos);
+//          //store the current position for the next frame
+//          previousTileCoordinate = tileCoordinate;
+//          //set the current tile to the highlighted version
+//          highlightMap.SetTile(tileCoordinate, highlightTile);
+//  }
+//       //This line outside the Update 
+// //   public TileBase highlightTile;
+  
+  
+// //   void Update() { 
+        
+// //       Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+// //       Vector3Int tileCoordinate = highlightMap.WorldToCell(mouseWorldPos);
+  
+// //       if(tileCoordinate != previousTileCoordinate ){
+// //           highlightMap.SetTile(previousTileCoordinate, null);
+// //           highlightMap.SetTile(tileCoordinate,highlightTile);
+// //           previousTileCoordinate = tileCoordinate;     
+// //       }
+  
+//     // Start is called before the first frame update
+
+
+
+//     // Update is called once per frame
+
+// }
